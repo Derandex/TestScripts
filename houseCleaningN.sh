a21431486ec8b66bfb3775a8f386f79b80dd1d17
Derandex <121528160+Derandex@users.noreply.github.com>
2024-08-06 15:06:51 -0500

    Update and rename cleanANDstore.sh to houseCleaningN.sh

--- houseCleaningN.sh
+++ houseCleaningN.sh
@@ -3,7 +3,7 @@ DIR="/home/gator/Documents/southCaptures"
 if [ "$(ls -A $DIR)" ]
 then
  echo "not empty"
- (cd /home/gator/Documents/southCaptures && ls -tp | grep -v '/$' | tail -n +800 | xargs -d '\n' -r rm --) ;
+ (cd /home/gator/Documents/northCaptures && ls -tp | grep -v '/$' | tail -n +800 | xargs -d '\n' -r rm --) ;
 else
  echo "Dir"
 fi
