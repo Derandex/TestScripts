a31769471e8deee31eb4a0d51b19660fa7dc6392
Derandex <121528160+Derandex@users.noreply.github.com>
2024-08-06 15:07:27 -0500

    Update and rename testDelete.sh to houseCleaningS.sh

--- houseCleaningS.sh
+++ houseCleaningS.sh
@@ -3,7 +3,7 @@ DIR="/home/gator/Documents/southCaptures"
 if [ "$(ls -A $DIR)" ]
 then
  echo "not empty"
- find "$DIR" -mtime +1 -exec rm {} \;
+ (cd /home/gator/Documents/southCaptures && ls -tp | grep -v '/$' | tail -n +800 | xargs -d '\n' -r rm --) ;
 else
  echo "Dir"
 fi
