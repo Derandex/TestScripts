0ae13610a2abc27760b4a858b9e59943f7d8e9e5
Derandex <121528160+Derandex@users.noreply.github.com>
2024-09-17 17:50:56 -0500

    Update houseCleaningS.sh

--- houseCleaningS.sh
+++ houseCleaningS.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-DIR="/home/gator/Documents/southCaptures"
+DIR="/home/USER/Documents/southCaptures"
 if [ "$(ls -A $DIR)" ]
 then
  echo "not empty"
- (cd /home/gator/Documents/southCaptures && ls -tp | grep -v '/$' | tail -n +800 | xargs -d '\n' -r rm --) ;
+ (cd /home/USER/Documents/southCaptures && ls -tp | grep -v '/$' | tail -n +800 | xargs -d '\n' -r rm --) ;
+ cp -a /home/USER/Documents/southCaptures/. /home/USER/Documents/persistSouth/
 else
  echo " Already Empty"
 fi
